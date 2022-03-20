5b0e99fdd6b06786c854da9c7bab6a857effee9b
Tristan Wise <tristan@tristanwise.com>
2022-03-20 16:39:47 -0400

    add in aoe liquipedia tournament scrape

--- postTopStreamersToSidebar.py
+++ postTopStreamersToSidebar.py
@@ -3,12 +3,13 @@ from reddit.api_calls import set_streamers_sidebar_widget, set_streamers_sidebar
 from facebook.scrape_calls import get_top_facebook_streamers
 import json
 
-top_twitch_streamers = get_top_twitch_streamers()
+#top_twitch_streamers = get_top_twitch_streamers()
 top_facebook_streamers = get_top_facebook_streamers()
+print('top_facebook_streamers', top_facebook_streamers)
 
-top_streams = top_twitch_streamers + top_facebook_streamers
-top_streams = sorted(top_streams, key=lambda d: int(d['viewers']), reverse=True)
-set_streamers_sidebar_widget(top_streams)
-set_streamers_sidebar_widget_for_old_reddit(top_streams)
+#top_streams = top_twitch_streamers + top_facebook_streamers
+#top_streams = sorted(top_streams, key=lambda d: int(d['viewers']), reverse=True)
+#set_streamers_sidebar_widget(top_streams)
+#set_streamers_sidebar_widget_for_old_reddit(top_streams)
 
 
